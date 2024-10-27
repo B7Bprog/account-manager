76959294dd922dda117c9c0c9cff3d622bd5cc98
B7Bprog <belabertalan@gmail.com>
2024-10-27 14:21:47 +0000

    feat: add delete account confirmation

--- src/components/SingleAccount.tsx
+++ src/components/SingleAccount.tsx
@@ -9,17 +9,45 @@ const SingleAccount: FC<{
 }> = ({ accounts, setAccounts }) => {
   const { accountId } = useParams<{ accountId: string }>();
   const [currentAccount, setCurrentAccount] = useState<Account>();
+  const [deleteAllowed, setDeleteAllowed] = useState(false);
+  const [deletePromptActive, setDeletePromptActive] = useState(false);
+
   useEffect(() => {
     setCurrentAccount(accounts.find((account) => account.id === accountId));
   }, [accountId, accounts]);
 
+  const handleDeleteButtonPress = () => {
+    if (!deleteAllowed) {
+      setDeletePromptActive(true);
+    }
+  };
+
   const handleDelete = () => {
+    setDeletePromptActive(false);
     setAccounts((accounts: Account[]) => {
       return accounts.filter((account) => {
         return account.id !== accountId;
       });
     });
   };
+
+  if (deletePromptActive) {
+    return (
+      <div className={styles.delete_prompt_wrapper}>
+        <h2 className={styles.prompt_message}>
+          Are you sure you want to delete the following account?
+        </h2>
+        <h2>{currentAccount?.accountName}</h2>
+        <div className={styles.button_wrapper}>
+          <button onClick={() => setDeletePromptActive(false)}>No</button>
+          <button id={styles.final_delete_button} onClick={handleDelete}>
+            Yes
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.accountListItem}>
       {currentAccount ? (
@@ -87,7 +115,10 @@ const SingleAccount: FC<{
             </div>
           </div>
           <div id={styles.delete_button_wrapper}>
-            <button id={styles.delete_account_button} onClick={handleDelete}>
+            <button
+              id={styles.delete_account_button}
+              onClick={handleDeleteButtonPress}
+            >
               Delete account
             </button>
           </div>
